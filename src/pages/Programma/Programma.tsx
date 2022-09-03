b8927d00da5ebd3cab02b0e286ded5a30fdb85da
GabrieleCorti <gabriele.corti.lavoro@gmail.com>
2022-09-03 17:08:36 +0200

    fix style programma

--- src/pages/Programma/Programma.tsx
+++ src/pages/Programma/Programma.tsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 import programmaFesta from "../../static/img/programmaFesta.png";
+import { Link } from "react-router-dom";
 
 const Programma: React.FC = () => {
+  const [loaded, setLoaded] = useState(false);
   return (
-    <div className="screen">
+    <div className="programma screen">
       <div className="programma-wrapper">
-        <img src={programmaFesta} alt="programma festa" />
+        <img
+          onLoad={() => setLoaded(true)}
+          src={programmaFesta}
+          alt="programma festa"
+          style={loaded ? {} : { display: "none" }}
+        />
+      </div>
+      <div style={{ margin: "1rem 0" }}>
+        <Link className="main-button" to={"/"} color={"white"}>
+          Ritorna a home
+        </Link>
       </div>
     </div>
   );
